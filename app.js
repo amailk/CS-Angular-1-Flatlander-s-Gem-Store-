3a0e854d919bef38dcb98653f59975b7e42e50ab
Ama Liyanage <liyaniama@gmail.com>
2017-01-13 13:48:58 -0500

    tabs

--- app.js
+++ app.js
@@ -13,12 +13,34 @@
     price: 2.95,
     description: '...',
     canPurchase: false,
-    soldOut: true
-  },
+    soldOut: true,
+    images: [
+      {
+        full: 'dode-full.jpg',
+        thumb: 'dode-thumb.jpg'
+      },
+      {
+        full: 'dode2-full.jpg',
+        thumb: 'dode2-thumb.jpg'
+      }
+      ]
+    },
   {
     name: "Pentagonal Gem",
     price: 5.95,
     description: "...",
     canPurchase: false,
+    soldOut:true,
+    images: [
+      {
+        full: 'pent-full.jpg',
+        thumb: 'pent-thumb.jpg'
+      },
+      {
+        full: 'pent2-full.jpg',
+        thumb: 'pent2-thumb.jpg'
+      }
+      ]
   }
+];
 })();
